f4459aa0dd07b90d3aa01f23bec8b81d3cb53ac3
luisresende <luisfrvilela@gmail.com>
2015-06-06 15:12:02 -0300

    Adding a custom font for splash screen text

--- app/src/main/java/com/modesteam/pardal/SplashScreenActivity.java
+++ app/src/main/java/com/modesteam/pardal/SplashScreenActivity.java
@@ -1,25 +1,37 @@
 package com.modesteam.pardal;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
+import android.util.Log;
 
 
 public class SplashScreenActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         ThreadData threadData = new ThreadData();
         Thread thread = new Thread(threadData);
         thread.start();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        TextView textViewVersion = (TextView) getWindow().findViewById(R.id.textViewVersion);
+        textViewVersion.setText("Pardal\n v 1.0");
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/futura.ttf");
+        textViewVersion.setTypeface(typeface);
 
         new Timer().schedule(new TimerTask() {
 
