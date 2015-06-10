b4a6e4ae69233d811f7022a9759396e48aaf3c41
Ricardo Canela <ricardo_canela123@hotmail.com>
2015-06-10 18:09:36 +0000

    Created reverse button on model list fragment

--- app/src/main/java/com/modesteam/pardal/ModelListFragment.java
+++ app/src/main/java/com/modesteam/pardal/ModelListFragment.java
@@ -12,6 +12,7 @@ import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
@@ -48,6 +49,7 @@ public class ModelListFragment extends Fragment implements AbsListView.OnItemCli
     private String mParam1;
     private String mParam2;
     private Model model;
+    private boolean changeButtonReverse = false;
 
     private OnFragmentInteractionListener mListener;
 
@@ -138,6 +140,24 @@ public class ModelListFragment extends Fragment implements AbsListView.OnItemCli
         EditText searchText = (EditText) view.findViewById(R.id.searchEditText);
 
         searchText.addTextChangedListener(ListViewSearch.searchListView(mAdapter));
+
+
+        final ImageButton ordenateButton = (ImageButton) view.findViewById(R.id.bOrdenate);
+        ordenateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                onReverseClick();
+                if (changeButtonReverse == false) {
+                    ordenateButton.setImageResource(R.drawable.arrow_up_float);
+                    changeButtonReverse = true;
+                } else {
+                    ordenateButton.setImageResource(R.drawable.arrow_down_float);
+                    changeButtonReverse = false;
+                }
+            }
+        });
+
 
         return view;
     }
