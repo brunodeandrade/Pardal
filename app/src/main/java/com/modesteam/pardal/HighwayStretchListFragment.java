e0d5652bb810d30bb2cf0bf2aa83b0affd685e71
Ricardo Canela <ricardo_canela123@hotmail.com>
2015-06-10 18:16:53 +0000

    Created reverse button on highwayStretch list fragment

--- app/src/main/java/com/modesteam/pardal/HighwayStretchListFragment.java
+++ app/src/main/java/com/modesteam/pardal/HighwayStretchListFragment.java
@@ -12,6 +12,7 @@ import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
@@ -46,6 +47,7 @@ public class HighwayStretchListFragment extends Fragment implements AbsListView.
     private String mParam1;
     private String mParam2;
     private HighwayStretch highwayStretch;
+    private boolean changeButtonReverse = false;
 
     //
     private OnFragmentInteractionListener mListener;
@@ -136,6 +138,22 @@ public class HighwayStretchListFragment extends Fragment implements AbsListView.
         mListView.setOnItemClickListener(this);
 
         searchText.addTextChangedListener(ListViewSearch.searchListView(mAdapter));
+
+        final ImageButton ordenateButton = (ImageButton) view.findViewById(R.id.bOrdenate);
+        ordenateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                onReverseClick();
+                if (changeButtonReverse == false) {
+                    ordenateButton.setImageResource(R.drawable.arrow_up_float);
+                    changeButtonReverse = true;
+                } else {
+                    ordenateButton.setImageResource(R.drawable.arrow_down_float);
+                    changeButtonReverse = false;
+                }
+            }
+        });
 
         return view;
     }
